18e8613b32de0d4a5602344615e2f4b4831f3e01
Maruf Khan <whoismaruf@outlook.com>
2019-07-27 11:45:19 +0600

    Added loops

---
+++ loops.dart
@@ -0,0 +1,14 @@
+void main() {
+  
+  //Default
+  List numbers = [];
+  int number;
+  for (int i = 0; i<5; i++ ){
+    numbers.add(i+1);
+  }
+  print(" ");
+  for (number in numbers){
+    print(number);
+  }
+
+}
