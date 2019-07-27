a26055a1e1d9b3bd8e7e44e3c0f504f75b1baccb
Maruf Khan <whoismaruf@outlook.com>
2019-07-27 11:29:58 +0600

    Added switch -case

--- conditional.dart
+++ conditional.dart
@@ -15,9 +15,23 @@ void main() {
   // Expression that can check any condition
   int age = 20;
   age > 18 ? print("Allowed") : print("Not Allowed");
-
   // Expression that only check, if the value is null or not
   String user = "Xisan";
   String guest = user ?? "Guest User";
   print(guest);
+
+  //Switch Case
+  int grade = 90;
+  switch (grade) {
+    case 80:
+    print("Average Result");
+      break;
+      
+    case 90:
+    print("Very Good");
+    break;
+
+    default:
+    print("It's Okay");
+  }
 }
