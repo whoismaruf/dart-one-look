06f9c0af44b4b5687f50266a52c1ea9268e96957
Maruf Khan <whoismaruf@outlook.com>
2019-07-28 23:51:06 +0600

    edited

--- function/function_a.dart
+++ function/function_a.dart
@@ -1,9 +1,22 @@
+//Normal Function
+// Function With Fat Arrow
+int myFatArrowFunction(int first, int second) => first+second;
+
 // Optional parameter
 //Function With Named Parameter
-int myNamedParamFunction([int price, int offer]){
+int myNamedParamFunction(int price, {int offer}){
   int final_price = price - offer;
   return final_price;
 }
 
-// Function With Fat Arrow
-int myFatArrowFunction(int first, int second) => first+second;
+//Default Parameter
+int myDefaultParamFunction(int a, {int b = 10}){
+  return a+b;
+}
+
+
+// Let's check them all
+void main(){
+  print(myNamedParamFunction(20 , offer : 2));
+  print(myDefaultParamFunction(20));
+}
